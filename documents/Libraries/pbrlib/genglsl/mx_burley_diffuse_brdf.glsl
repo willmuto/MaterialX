9e3329cd5865dbbdac19835739bbe81da7fbd954
Jonathan Stone <jstone@lucasfilm.com>
2019-03-07 22:51:37 -0800

    Update Burley Diffuse to latest naming convention

--- documents/Libraries/pbrlib/genglsl/mx_burley_diffuse_brdf.glsl
+++ documents/Libraries/pbrlib/genglsl/mx_burley_diffuse_brdf.glsl
@@ -1,6 +1,6 @@
 #include "pbrlib/genglsl/lib/mx_bsdfs.glsl"
 
-void mx_burleydiffusebrdf_reflection(vec3 L, vec3 V, float weight, vec3 color, float roughness, vec3 normal, out BSDF result)
+void mx_burley_diffuse_brdf_reflection(vec3 L, vec3 V, float weight, vec3 color, float roughness, vec3 normal, out BSDF result)
 {
     float NdotL = dot(L, normal);
     if (NdotL <= 0.0 || weight < M_FLOAT_EPS)
@@ -13,7 +13,7 @@ void mx_burleydiffusebrdf_reflection(vec3 L, vec3 V, float weight, vec3 color, f
     return;
 }
 
-void mx_burleydiffusebrdf_indirect(vec3 V, float weight, vec3 color, float roughness, vec3 normal, out vec3 result)
+void mx_burley_diffuse_brdf_indirect(vec3 V, float weight, vec3 color, float roughness, vec3 normal, out vec3 result)
 {
     if (weight < M_FLOAT_EPS)
     {
